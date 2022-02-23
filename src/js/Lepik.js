07cd9ea953f3e84154df12a95dcbaac8f2cdaa73
borecjeborec1 <robingrundel@seznam.cz>
2022-02-23 15:05:41 +0100

    secured types

--- src/js/Lepik.js
+++ src/js/Lepik.js
@@ -9,6 +9,10 @@ class Lepik {
         this.hasGoodVersion = hasGoodVersion;
     }
     mouseMove(x = 0, y = 0, a = false, d = 0.2) {
+        if (typeof x !== "number") return console.log("x parameter must be a number")
+        if (typeof y !== "number") return console.log("y parameter must be a number")
+        if (typeof a !== "boolean") return console.log("absolute parameter must be a boolean")
+        if (typeof d !== "number") return console.log("delay parameter must be a number")
         this.#changeCurrent(`mouseMove(${x},${y},${a ? "True" : "False"},${d})`);
         if (this.safeMode) this.#rfc()
     }
@@ -16,16 +20,29 @@ class Lepik {
         this.mouseClick(key, 2)
     }
     mouseClick(key = "left", am = 1) {
+        if (key == 0) key = "left";
+        if (key == 1) key = "right";
+        if (key == 2) key = "middle";
         key = key.toLowerCase();
         am = Math.abs(am)
         this.#changeCurrent(`mouseClick('${key}',${am})`);
         if (this.safeMode) return this.#rfc()
     }
     mouseDrag(fx = 0, fy = 0, tx = 10, ty = 10, a = false, d = 0.2) {
+        if (typeof fx !== "number") return console.log("fromX parameter must be a number")
+        if (typeof fy !== "number") return console.log("fromY parameter must be a number")
+        if (typeof tx !== "number") return console.log("toX parameter must be a number")
+        if (typeof ty !== "number") return console.log("toY parameter must be a number")
+        if (typeof a !== "boolean") return console.log("absolute parameter must be a boolean")
+        if (typeof d !== "number") return console.log("delay parameter must be a number")
         this.#changeCurrent(`mouseDrag(${fx},${fy},${tx},${ty},${a ? "True" : "False"},${d})`);
         if (this.safeMode) this.#rfc()
     }
     mouseScroll(am = 1) {
+        if (typeof am !== "number") {
+            console.log("am parameter should be a number, using default value 1")
+            am = 1
+        }
         this.#changeCurrent(`mouseScroll(${am})`);
         if (this.safeMode) this.#rfc()
     }
@@ -42,10 +59,14 @@ class Lepik {
 
 
     keyTap(key = "a") {
+        if (typeof key !== "string") return console.log("key parameter must be a string, use lepik.write() to write numbers")
         this.#changeCurrent(`keyTap('${key}')`);
         if (this.safeMode) this.#rfc()
     }
     write(msg = "Hello From LepikJS", d = 0.1) {
+        if (typeof d !== "number") d = 0.1
+        msg = msg.toString();
+
         let arSending = msg.split(" ");
         for (let i = 0; i < arSending.length; i++) {
             arSending[i] = '\\"' + arSending[i] + '\\"';
@@ -71,6 +92,9 @@ class Lepik {
                 lepikEvents.events.on("mouseClick", data => {
                     cb({ x: data[0], y: data[1], button: data[2] })
                 })
+                break;
+            default:
+                console.log("Unknown event: " + ev)
                 break;
         }
     }
