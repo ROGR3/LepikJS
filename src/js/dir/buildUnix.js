eae4fff67c17ad2ec0f42549e767a37d7d601c88
borecjeborec1 <robingrundel@seznam.cz>
2022-02-25 17:42:45 +0100

    postinstall try catch block

--- src/js/dir/buildUnix.js
+++ src/js/dir/buildUnix.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
-
-fs.readdirSync(__dirname + "/../../py").forEach(file => {
-  if (file.endsWith(".py")) {
-    fs.copyFile("src/py/" + file, "lib/" + file, (err) => {
-      if (err) throw err;
-    });
-  }
-})
+try {
+  fs.readdirSync(__dirname + "/../../py").forEach(file => {
+    if (file.endsWith(".py")) {
+      fs.copyFile("src/py/" + file, "lib/" + file, (err) => {
+        if (err) throw err;
+      });
+    }
+  })
+} catch (er) {
+  console.log(er)
+}
