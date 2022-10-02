3a5558a7cbd9af5b2d5c217cce47278366bce25d
borecjeborec1 <robingrundel@seznam.cz>
2022-10-02 13:42:29 +0200

    lepikevents@1.6.1

--- typescript/Lepik.ts
+++ typescript/Lepik.ts
@@ -3,11 +3,12 @@ import { execSync } from "child_process";
 
 class Lepik {
   private pyCommand: string = "";
+  private safeMode: boolean;
   private readonly pyPath: string;
   private readonly isWin: boolean;
-  private safeMode: boolean;
   private readonly hasGoodVersion: boolean;
   private readonly supportedChars: string[] = [`enter`, `backspace`, `capslock`, `tab`, `space`, `left`, `up`, `right`, `down`, `insert`, `delete`, `0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`, `f1`, `f2`, `f3`, `f4`, `f5`, `f6`, `f7`, `f8`, `f9`, `f10`];
+
   constructor(obj: { _path: string, _isWin: boolean, _hasGoodVersion: boolean }) {
     this.pyPath = obj._path;
     this.isWin = obj._isWin;
