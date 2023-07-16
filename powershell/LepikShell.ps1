9df6d17e5ef5bdc8c25271a35dd30cd258ef1340
borecjeborec1 <robingrundel@seznam.cz>
2023-07-16 08:52:08 +0200

    getWindowSize

--- powershell/LepikShell.ps1
+++ powershell/LepikShell.ps1
@@ -40,6 +40,9 @@ public static class User32 {
 
     [DllImport("user32.dll")]
     public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+    
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
     [DllImport("user32.dll", SetLastError = true)]
     public static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
